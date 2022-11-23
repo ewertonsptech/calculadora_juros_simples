12ec747e190aa611005735b51134f2a4db72583e
ewerton <ewerton.lima@sptech.school>
2022-11-23 15:21:31 -0300

    att

---
+++ script.js
@@ -0,0 +1,16 @@
+function caclular(){
+    let c = Number(capital.value)
+    let i = Number(taxa_mes.value)
+    let n = Number(meses_divida.value)
+    let i_total = i / 100
+    let juros_total = c*(i_total*n)
+    
+    resultado.innerHTML = ''
+
+    if(i <=0 || c<=0 || n <=0 ){
+        alert("Numero inválido tente novamente")
+    }
+  else{
+    resultado.innerHTML = `O Juros ao Total de ${i} Meses Ficaram no Total de R$ ${juros_total.toFixed(1)}`
+}
+}
